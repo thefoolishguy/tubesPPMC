8ffb8f9ad8f84fca098267777e427178ab3f9102
Martinus William Hartono <martinus.wh@gmail.com>
2020-04-15 11:56:47 +0700

    Final 15 April 2020

---
+++ Final-15 April 2020/lib_conway.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "lib_conway.c"
+
+/*  Fungsi tick akan mencetak generasi selanjutnya dari Conway's Game of Life
+    ke console.
+    Input   : pointer of 2d array, row, column
+*/
+void tick (char **arr, int row, int column);
+void delay(int number_of_milli_seconds);
--- Final-15 April 2020/main_conway.c
+++ Final-15 April 2020/main_conway.c
@@ -1,36 +1,43 @@
 /*  EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
- *  MODUL 8 â€“ TUGAS BESAR
- *  Kelompok            : 1
- *  Hari dan Tanggal    : Rabu, 7 April 2020
+ *  MODUL 8 – TUGAS BESAR
+ *  Kelompok            : 5C
+ *  Hari dan Tanggal    : Rabu, 15 April 2020
  *  Asisten (NIM)       : Berry (13216027)
- *  Nama File           : menu_test.c
+ *  Nama File           : menu_conway.c
  *  Deskripsi           : Main menu program Conway's Game of Life
  */
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "tick.h"
-
+#include <time.h>
+#include "lib_conway.h"
 
 char buff[255], store[10], content;
-int i, j, row, column;
+int i, j, row, column, n_animate, k, m;
 FILE *fp;
 
 int main(void)
 {
-    int choice;                //1. Animate, 2. Tick, 3. Quit
+    int choice;                 //1. Animate, 2. Tick, 3. Quit
     int quit=0;
     char filename[20];          //File name
 
-    printf("PROGRAM CONWAY'S GAME OF LIFE BY KELOMPOK C5\n\n");
+    printf("----------PROGRAM CONWAY'S GAME OF LIFE BY KELOMPOK C5----------\n\n");
+    printf("Program ini merupakan sebuah simulator perkembangan suatu populasi\n");
+    printf("berdasarkan aturan tertentu. Input file merupakan file eksternal .txt\n");
+    printf("yang berisi array yang memodelkan populasi tersebut. Sel mati dimodelkan\n");
+    printf("sebagai - dan sel hidup sebagai X. Pada setiap buah iterasi, populasi\n");
+    printf("akan mengalami perubahan state mengikuti empat buah kondisi, yaitu\n");
+    printf("underpopulation, next generation, overpopulation, dan reproduction.\n");
+    printf("\nSelamat bermain!\n");
+
 
     while(quit==0)
     {
-        printf("Silakan masukkan file seed .txt yang diinginkan: "); scanf("%s", &filename);
+        printf("\nSilakan masukkan file seed .txt yang diinginkan: "); scanf("%s", &filename);
 
-        //Prosedure assign dari file eksternal ke matrix
-        printf("run prosedur assign file eksternal ke matrix\n\n");
+        //Assign dari file eksternal ke matrix dalam program
         fp = fopen(filename, "r");
 
         fscanf(fp,"%s",store);
@@ -44,7 +51,8 @@ int main(void)
             arr[i] = (char *)malloc(column * sizeof(int));
         };
 
-        printf("Isi file %s adalah:", filename);
+        //Memunculkan isi matrix dalam program
+        printf("\nIsi file %s adalah:", filename);
         for (i = 0; i < row; i++)
         {
             for (j = 0; j < column+1; j++)
@@ -72,45 +80,32 @@ int main(void)
         {
             if(choice==1)
             {
-                //run prosedur animate
-                int k,m;
-                printf("Masukkan jumlah iterasi animate: ");
-                scanf(" %d",&m);
-                //Mencetak isi awal
+                //run animate
+                printf("Masukkan jumlah iterasi animate: ");scanf("%d",&m);
                 system("cls");
-                for (i = 0; i < row; i++)
-                        {
-                            for (j = 0; j < column+1; j++)
-                            {printf("%c",arr[i][j]);
-                            };
-                        };
-                printf("\nSeed Awal");
-                //iterasi tick
-                delay(1000);
-                    for(k=0;k<m;k++){
+
+                for(k=0;k<m;k++)
+                {
                     system("cls");
                     tick(arr,row,column);
                     printf("\nIterasi ke %d",k+1);
-                    delay(1000);
-                    }
+                    delay(250);
+                };
             }
             else if(choice==2)
             {
-                //run prosedur tick
-                printf("run prosedur tick\n");
+                //run tick
                 tick(arr, row, column);
             }
             else
             {
                 printf("Input salah silakan input ulang.\n");
             };
-            printf("\n1. Animate (Melakukan animasi pada seed sebanyak jumlah iterasi yang dimasukkan).\n");
-            printf("2. Tick (Melakukan simulasi pada seed sebanyak satu iterasi).\n");
-            printf("3. Quit (Keluar dari simulasi untuk melakukan input file seed lain atau keluar dari program).\n");
             printf("\n\nMasukkan pilihan: "); scanf("%d", &choice);
         };
         printf("Apakah mau keluar atau pilih filename lain? (1/0): "); scanf("%d", &quit);
-    }
+    };
+    printf("Terimakasih telah memainkan program Conway's Game of Life!\n");
 
     fclose(fp);
     return 0;
